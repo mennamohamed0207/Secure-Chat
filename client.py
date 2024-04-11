cde80f3e5809524ae884e2aa718459e6129f2083
Sara Bisheer <113303945+SH8664@users.noreply.github.com>
2024-04-11 12:24:44 +0200

    initial chat

--- client.py
+++ client.py
@@ -1,6 +1,28 @@
 import socket
 import threading
 from commonFunctions import *
+
+
+class ElGamal:
+    __init__ = None
+
+    @staticmethod
+    def generate_keys():
+        return 333, 444
+    
+    @staticmethod
+    def encrypt(message):
+        return message
+
+    @staticmethod
+    def verify(message):
+        return False
+
+    @staticmethod
+    def decrypt(message):
+        return message
+        
+
 def client():
     # Get the hostname of the server
     host = socket.gethostname()
@@ -18,10 +40,43 @@ def client():
         print("Server not found")
         return
     
-    s.sendall(f"Hello, Server!")
-    threading.Thread(target=receive, args=(s,)).start()
-    send(s)
-    s.close()
+    # Generate a public and private key
+    public_key, private_key = ElGamal.generate_keys()
+
+    # Send the public key to the server
+    s.sendall(str(public_key).encode())
+    print("You sent : ",public_key)
+
+    # Receive the public key from the client
+    secret_key=int(s.recv(2048).decode("utf-8"))
+    print("You received : ",secret_key)
+
+    threading.Thread(target=send_msg, args=(s,)).start()
+    threading.Thread(target=receive_msg, args=(s,)).start()
+    # send(s)
+    # s.close()
+
+def send_msg(c):
+    while 1:
+        # Send messages from the client
+        message = input()
+        message = ElGamal.encrypt(message).encode()
+        c.sendall(message)
+
+def receive_msg(c):
+    while 1:
+        # Listen for messages from the client       
+        message=c.recv(2048).decode("utf-8")
+        if message == "exit":
+            print("the connection is closed")
+            break
+        if ElGamal.verify(message)==False:
+            print("Untrusted Connection")
+            c.sendall("exit".encode())
+            break
+        print("You RECEIVED:",ElGamal.decrypt(message))
+    c.close()    
+
 
 if __name__ == "__main__":
     # Start the client
--- server.py
+++ server.py
@@ -1,6 +1,31 @@
 import socket
 import threading 
 from commonFunctions import *
+
+
+
+class ElGamal:
+    __init__ = None
+
+    @staticmethod
+    def generate_keys():
+        return 333, 444
+    
+    @staticmethod
+    def encrypt(message):
+        return message
+
+    @staticmethod
+    def verify(message):
+        return False
+
+    @staticmethod
+    def decrypt(message):
+        return message
+        
+
+
+
 def server():
     # Get the hostname of the server
     host = socket.gethostname()
@@ -22,11 +47,49 @@ def server():
     # Listen for incoming connections, allowing up to 2 clients in the queue
     s.listen(2)
 
+
     # Accept an incoming connection
     c, address = s.accept()
-    threading.Thread(target=receive, args=(c,)).start()
-    send(c)
-    c.close()
+
+    
+    # Generate a public and private key
+    public_key, private_key = ElGamal.generate_keys()
+
+
+    # Receive the secret key from the server
+    secret_key=int(c.recv(2048).decode("utf-8").split("\n")[0])
+    print("You received : ",secret_key)
+
+    # Send the public key to the client
+    c.sendall(str(public_key).encode())
+    print("You sent : ",public_key)
+
+
+    threading.Thread(target=send_msg, args=(c,)).start()
+    threading.Thread(target=receive_msg, args=(c,)).start()
+    # send(c)
+    # c.close()
+
+def send_msg(c):
+    while 1:
+        # Send messages from the client
+        message = input()
+        message = ElGamal.encrypt(message).encode()
+        c.sendall(message)
+
+def receive_msg(c):
+    while 1:
+        # Listen for messages from the client
+        message=c.recv(2048).decode("utf-8")
+        if message == "exit":
+            print("the connection is closed")
+            break
+        if ElGamal.verify(message)==False:
+            print("Untrusted Connection")
+            c.sendall("exit".encode())
+            break
+        print("You RECEIVED:",ElGamal.decrypt(message))
+    c.close() 
 
 
 if __name__ == "__main__":
